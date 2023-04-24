95f3c0a9a040b4a9872adf537308fb0d1b5293c3
DVastMasters <contatorafaeldutra@outlook.com>
2023-04-24 09:58:35 -0300

    ruby basic numbers manipulation

---
+++ numbers.rb
@@ -0,0 +1,33 @@
+puts "--INTEGER--"
+puts 19
+puts "--NEGATIVE--"
+puts -19
+puts "--FLOAT--"
+# 15 float digits?
+puts 19.1598775154454564897 # 19.159877515445455
+puts 19.159999999999999999 # 19.16
+puts 19.1234567891011121314151617181920 # 19.123456789101112
+
+puts "--MUTIPLY--"
+puts 2*3
+puts "--EXPONENTIAL--"
+puts 2**3
+puts "--REMAINDER--"
+puts 10 % 3
+
+
+num = -20
+puts ("my fav num is " + num.to_s)
+puts num.abs()
+
+num = 20.26
+puts num.round()
+
+puts num.ceil()
+puts num.floor()
+
+puts Math.sqrt(36)
+puts Math.log(8)
+
+puts 1 + 7
+puts 1.0 + 7
