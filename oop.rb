76f5d81df687947b5581d12637468478852f5283
DVastMasters <contatorafaeldutra@outlook.com>
2023-04-25 21:36:36 -0300

    ruby oop

---
+++ oop.rb
@@ -0,0 +1,12 @@
+class Book
+    attr_accessor :title, :author, :pages
+
+    def initialize(title, author, pages)
+        @title = title
+        @author = author
+        @pages = pages
+    end
+end
+
+book1 = Book.new("JK Rowling", "JK Rowling", 400)
+book2 = Book.new("Lord of the rings", "Tolkien", 500)
