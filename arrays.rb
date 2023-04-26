f587dcd15aac192a28a7cc7f006917e2a74ccdc9
DVastMasters <contatorafaeldutra@outlook.com>
2023-04-25 21:33:33 -0300

    array and hashes

---
+++ arrays.rb
@@ -0,0 +1,18 @@
+friends = Array["Kevin", "Karen", "Oscar"]
+puts friends
+
+puts friends[0]
+
+puts friends[0, 2]
+
+friends[0] = "Dwight"
+
+animals = Array.new
+animals[0] = "Dog"
+animals[1] = "Cat"
+puts animals
+
+puts animals.length
+puts animals.include? "Dog"
+puts animals.reverse()
+puts animals.sort()
---
+++ hashes.rb
@@ -0,0 +1,10 @@
+states = {
+    :Pennsylvania => "PA",
+    "New York" => "NY",
+    "Oregon" => "OR"
+}
+
+puts states
+
+puts states["New York"]
+puts states[:Pennsylvania]
