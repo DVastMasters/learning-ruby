405b75d0defccb3f109e91f538e1b9d9c1682e00
DVastMasters <contatorafaeldutra@outlook.com>
2023-05-22 11:15:32 -0300

    Handling exceptions in Ruby

---
+++ odin_project_ruby_path/3_oop_basics/handling_errors/exceptions_classes.rb
@@ -0,0 +1,48 @@
+=begin
+Exception family tree
+
+Exception
+    NoMemoryError
+    ScriptError
+        LoadError
+        NotImplementedError
+        SyntaxError
+    SignalException
+        Interrupt
+    StandardError
+        ArgumentError
+        IOError
+            EOFError
+        IndexError
+            StopIteration
+        LocalJumpError
+        NameError
+            NoMethodError
+        RangeError
+            FloatDomainError
+        RegexpError
+        RuntimeError
+        SecurityError
+        SystemCallError
+        SystemStackError
+        ThreadError
+        TypeError
+        ZeroDivisionError
+    SystemExit
+    fatal   
+=end
+
+
+
+while 1
+  puts "Enter a number>>"
+  begin
+    num = Kernel.gets.match(/\d+/)[0]
+  rescue StandardError=>e
+    puts "Erroneous input!"
+    puts e
+    puts "\tTry again...\n"   
+  else
+    puts "#{num} + 1 is: #{num.to_i + 1}"
+  end
+end
